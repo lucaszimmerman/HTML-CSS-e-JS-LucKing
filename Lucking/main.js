a49db9bbce612a2bf6185ff8156506d0e051d4df
lucaszimmerman <lucaszimmermants@gmail.com>
2023-05-18 14:27:18 -0300

    post html

--- Lucking/main.js
+++ Lucking/main.js
@@ -4,3 +4,5 @@ let header =  document.querySelector("header");
 window.addEventListener("scroll", () => {
     header.classList.toggle("shadow", window.scrollY > 0)
 })
+
+// Isso permite que você defina estilos diferentes para o cabeçalho com e sem a classe "shadow", criando um efeito visual quando o usuário faz a rolagem da página.
